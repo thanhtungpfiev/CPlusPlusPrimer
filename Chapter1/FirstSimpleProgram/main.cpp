49a8e4ce33e388fb89a97111b707caa83e504524
thanhtungpfiev <thanhtungpfiev@gmail.com>
2022-03-15 21:37:21 +0700

    First Simple Program

---
+++ Chapter1/FirstSimpleProgram/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * author thanhtung thanhtungpfiev@gmail.com
+ *
+ * date 15 - 3 - 2022
+ *
+ * class %{Cpp:License:ClassName}
+ *
+ * brief
+ *
+ *
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
