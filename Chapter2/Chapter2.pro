a4ad34ef65e7f76d1d8b5bb589317db7ad2a03ee
thanhtungpfiev <thanhtungpfiev@gmail.com>
2022-04-01 07:47:35 +0700

    Finish Compound Types

--- Chapter2/Chapter2.pro
+++ Chapter2/Chapter2.pro
@@ -1,5 +1,6 @@
 TEMPLATE = subdirs
 
 SUBDIRS += \
+    Exercise2-16 \
     Exercise2-3 \
     Exercise2-8
---
+++ Chapter2/Exercise2-16/main.cpp
@@ -0,0 +1,25 @@
+﻿/**
+ * author thanhtung thanhtungpfiev@gmail.com
+ *
+ * date 23 - 3 - 2022
+ *
+ * class %{Cpp:License:ClassName}
+ *
+ * brief
+ *
+ *
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0, &r1 = i;
+    double d = 0, &r2 = d;
+    r2 = 3.14159;
+    r2 = r1;
+    i = r2;
+    r1 = d;
+    return 0;
+}
